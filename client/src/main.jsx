e79a7b01b88ac7425fca8bd14395363b688d5732
rohanz2003 <zenderohan2012@gmail.com>
2026-07-29 22:59:55 +0530

    Add Vercel SPA config and production API base URL

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || '';
+axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
